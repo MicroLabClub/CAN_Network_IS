5851fd43d373541f5f967fb7780bb548805fe49b
MicroLabClub <63069279+MicroLabClub@users.noreply.github.com>
2023-10-26 18:55:36 +0300

    added control service unit stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "Arduino.h"
 #include "dd_can_mcp/dd_can_mcp.h"
 #include "dd_can_mcp/dd_can_matrix.h"
-#include "dd_joystick/dd_joystick.h"
 #include "dd_can_mcp/dd_can_signal.h"
+#include "dd_joystick/dd_joystick.h"
+#include "dd_buzzer/dd_buzzer.h"
+#include "srv_control/srv_control.h"
 
 void setup()
 {
@@ -15,27 +17,45 @@ void setup()
 #define SYS_TICK 1
 
 #define CAN_SEND_REC (5000 / SYS_TICK)
-#define CAN_RECV_REC (1 / SYS_TICK)
-#define CAN_PRINT_REC (2000 / SYS_TICK)
+int send_rec_cnt = CAN_SEND_REC+1;
 
-int send_rec_cnt = CAN_SEND_REC;
-int recv_rec_cnt = CAN_RECV_REC;
-int print_rec_cnt = CAN_PRINT_REC;
+#define CAN_RECV_REC (1 / SYS_TICK)
+int recv_rec_cnt = CAN_RECV_REC+2;
+
+#define CAN_PRINT_REC (2000 / SYS_TICK)
+int print_rec_cnt = CAN_PRINT_REC+3;
 
 #define DD_JOYSTICK_REC (10 / SYS_TICK)
-int dd_joystick_rec_cnt = DD_JOYSTICK_REC;
+int dd_joystick_rec_cnt = DD_JOYSTICK_REC+3;
+
+#define DD_BUZZER_REC (10 / SYS_TICK)
+int dd_buzzer_rec_cnt = DD_BUZZER_REC+4;
+
+#define SRV_CONTROL_REC (10 / SYS_TICK)
+int srv_control_rec_cnt = SRV_CONTROL_REC+5;
 
 
 void loop()
 {
   if (--dd_joystick_rec_cnt <= 0)
-  {// send data per 100ms
+  {// send data per 10ms
     dd_joystick_loop();
     dd_joystick_rec_cnt = DD_JOYSTICK_REC;
   }  
   
-  if (--send_rec_cnt <= 0)
+  if (--dd_buzzer_rec_cnt <= 0)
+  {// send data per 10ms
+    dd_buzzer_loop();
+    dd_buzzer_rec_cnt = DD_BUZZER_REC;
+  }  
 
+  if (--srv_control_rec_cnt <= 0)
+  {// send data per 10ms
+    srv_control_loop();
+    srv_control_rec_cnt = SRV_CONTROL_REC;
+  }  
+  
+  if (--send_rec_cnt <= 0)
   {// send data per 100ms
     dd_can_send_loop();
     send_rec_cnt = CAN_SEND_REC;
@@ -52,7 +72,7 @@ void loop()
   {// print data per 100ms
     dd_can_matrix_print();
     dd_joystick_report();
-        dd_can_tx_buff_print();
+    dd_can_tx_buff_print();
 
     print_rec_cnt = CAN_PRINT_REC;
   }
---
+++ src/srv_control/srv_control.cpp
@@ -0,0 +1,11 @@
+#include "srv_control.h"
+
+
+void srv_control_setup(){
+
+}
+
+
+void srv_control_loop(){
+
+}
